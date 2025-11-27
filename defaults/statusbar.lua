86f2a48e5c155be1703f99867a1874da4d01568d
Kaleb Dickerson <65053946+Prometheus1400@users.noreply.github.com>
2025-11-27 10:20:31 -0600

    feat: add git branch to lua status line default (#35)

--- defaults/statusbar.lua
+++ defaults/statusbar.lua
@@ -4,11 +4,20 @@ ui.status_line = {
 			get_active_session(),
 		},
 		b = {
+			function()
+				local s = os.date("%Y-%m-%d %H:%M:%S")
+				return s
+			end,
 		},
 		c = {
 			function()
-        local s = os.date("%Y-%m-%d %H:%M:%S")
-        return s
+				local f = io.popen("git rev-parse --abbrev-ref HEAD 2>/dev/null")
+				if not f then
+					return nil
+				end
+				local branch = f:read("*l")
+				f:close()
+				return branch
 			end,
 		},
 	},
